1d8fec63843c995534674d7fbe177d77033d2224
Ryan Wooden <rygwdn@gmail.com>
2020-10-12 13:12:39 -0300

    cleanup in midiclient.

--- obsMidiClient.py
+++ obsMidiClient.py
@@ -6,17 +6,11 @@ import obspython as obs
 
 def testInput(cmdType, channel, value):
     global midiParams
-    scenes = obs.obs_frontend_get_scenes()
-    transitions = obs.obs_frontend_get_transitions()
     
-    
-    if midiParams[0] == cmdType and midiParams[1] == channel:
-        transition()
-    if midiParams[2] == cmdType and channel >= midiParams[3] and channel < midiParams[3] + len(scenes):
-        setPreview(channel - midiParams[3])
-    if midiParams[4] == cmdType and channel >= midiParams[5] and channel < midiParams[5] + len(transitions):
-        setTransition(channel - midiParams[5])
-    if midiParams[6]:
+    if transitionControlType == cmdType and channel >= transitionFirst:
+        print("do transition")
+        transition(channel - transitionFirst)
+    if logMidiInput:
         print(cmdType+"\t"+str(channel)+"\t"+str(value))
     
 
@@ -28,7 +22,10 @@ def controlChange(channel, value):
 
 currentMidiPort = "";
 midiin = None
-midiParams = ["",0,"",0,"",0, False];
+logMidiInput = False
+transitionControlType = ""
+transitionFirst = 0
+transitionTime = 0
 
 class MidiInputHandler(object):
     def __init__(self, port):
@@ -53,28 +50,15 @@ def script_properties():
     midiIn = rtmidi.MidiIn()
     availablePorts = midiIn.get_ports()
     obs.obs_property_list_add_string(devicesList, "", "")
-    for port in availablePorts:    
+    for port in availablePorts:
         obs.obs_property_list_add_string(devicesList, port, port)
-
-    transitionMidiType = obs.obs_properties_add_list(props, "transitionMidiType", "Transition midi type", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-    obs.obs_property_list_add_string(transitionMidiType, "Control Change", "CC")
-    obs.obs_property_list_add_string(transitionMidiType, "Note On", "Note")
     
-    transitionAddress = obs.obs_properties_add_int(props, "transitionMidiAddress", "Transition Midi Address", 0, 127, 1)
-    
-    
-    scenesMidiType = obs.obs_properties_add_list(props, "scenesMidiType", "Scene set midi type", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-    obs.obs_property_list_add_string(scenesMidiType, "Control Change", "CC")
-    obs.obs_property_list_add_string(scenesMidiType, "Note On", "Note")
-    
-    scenesAddress = obs.obs_properties_add_int(props, "scenesMidiAddress", "First scene midi address", 0, 127, 1)
-    
-    
-    transitionsMidiType = obs.obs_properties_add_list(props, "transitionsMidiType", "Transition set midi type", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    transitionsMidiType = obs.obs_properties_add_list(props, "transitionToSceneMidiType", "Transition to Scene midi type", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     obs.obs_property_list_add_string(transitionsMidiType, "Control Change", "CC")
     obs.obs_property_list_add_string(transitionsMidiType, "Note On", "Note")
 
-    transitionsAddress = obs.obs_properties_add_int(props, "transitionsMidiAddress", "First transition Midi Address", 0, 127, 1)
+    obs.obs_properties_add_int(props, "transitionToSceneMidiAddress", "First Transition to Scene Midi Address", 0, 127, 1)
+    obs.obs_properties_add_int(props, "transitionTime", "Transition time (MS)", 0, 5000, 1)
     
     obs.obs_properties_add_bool(props, "logMidiInput", "Log MIDI input")
     
@@ -87,7 +71,11 @@ def script_properties():
 def script_update(settings):
     global currentMidiPort
     global midiin
-    global midiParams
+    global logMidiInput
+    global transitionControlType
+    global transitionFirst
+    global transitionTime
+
     askedPort = obs.obs_data_get_string(settings, "midiDevice")
     if currentMidiPort != askedPort:
         if currentMidiPort != "":
@@ -96,20 +84,19 @@ def script_update(settings):
         ok = True
         if askedPort != "":
             try:
-                midiin, port_name = open_midiinput(askedPort)
+                midiin, port_name = open_midiinput(askedPort, use_virtual=True)
                 midiin.set_callback(MidiInputHandler(port_name))
+                print("connected to " + port_name)
             except (EOFError, KeyboardInterrupt):
                 print("Meh :/")
                 ok = False
             if ok:
                 currentMidiPort = askedPort
-    midiParams[0] = obs.obs_data_get_string(settings, "transitionMidiType")
-    midiParams[1] = obs.obs_data_get_int(settings, "transitionMidiAddress")
-    midiParams[2] = obs.obs_data_get_string(settings, "scenesMidiType")
-    midiParams[3] = obs.obs_data_get_int(settings, "scenesMidiAddress")
-    midiParams[4] = obs.obs_data_get_string(settings, "transitionsMidiType")
-    midiParams[5] = obs.obs_data_get_int(settings, "transitionsMidiAddress")
-    midiParams[6] = obs.obs_data_get_bool(settings, "logMidiInput")
+
+    transitionControlType = obs.obs_data_get_string(settings, "transitionToSceneMidiType")
+    transitionFirst = obs.obs_data_get_int(settings, "transitionToSceneMidiAddress")
+    logMidiInput = obs.obs_data_get_bool(settings, "logMidiInput")
+    transitionTime = obs.obs_data_get_int(settings, "transitionTime")
     
 
 def script_unload():
@@ -118,26 +105,15 @@ def script_unload():
     if currentMidiPort != "":
         midiin.close_port();
 
-
-def setTransition(num):
-    transitions = obs.obs_frontend_get_transitions()
-    if num < len(transitions) and num >= 0:
-        obs.obs_frontend_set_current_transition(transitions[num])
-        
-def transition(num = -1):
-    trans = None
-    if num >= 0 :
-        setTransition(num)
+def transition(num):
     trans = obs.obs_frontend_get_current_transition()
-    act = obs.obs_frontend_get_current_scene()
-    mode = obs.OBS_TRANSITION_MODE_AUTO
-    duration = 0
-    dest = obs.obs_frontend_get_current_preview_scene()
-    obs.obs_transition_start(trans, mode, duration, dest)
-    obs.obs_frontend_set_current_scene(dest)
-    obs.obs_frontend_set_current_preview_scene(act)
 
-def setPreview(num):
+    duration = transitionTime #obs.obs_frontend_get_transition_duration()
     scenes = obs.obs_frontend_get_scenes()
-    if num < len(scenes) and num >= 0:
-        obs.obs_frontend_set_current_preview_scene(scenes[num])
+    if num > len(scenes):
+        print(f"Invalid scene number: {num}")
+        return
+    print(f"want to transition to: {num}")
+    dest = scenes[num]
+    
+    obs.obs_transition_start(trans, obs.OBS_TRANSITION_MODE_AUTO, duration, dest)
