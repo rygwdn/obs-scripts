87b6a76394237844e3be88b6b667a97277a645e0
Ryan Wooden <rygwdn@gmail.com>
2020-11-11 22:44:17 -0400

    more cleanup.

--- obsMidiClient.py
+++ obsMidiClient.py
@@ -1,10 +1,136 @@
-# MIDI !
+# https://spotlightkid.github.io/python-rtmidi/rtmidi.html
 import rtmidi
 from rtmidi.midiutil import open_midiinput
 
+# https://obsproject.com/docs/reference-frontend-api.html
 import obspython as obs
 
+
+# defines script description 
+def script_description():
+    return '''Select preview scenes and trigger transition with a midi controller. '''
+
+# defines user properties
+def script_properties():
+    props = obs.obs_properties_create()
+
+    Midi.addConfig(props)
+    TransitionHandler.addConfig(props)
+    RecordingHandler.addConfig(props)
+
+    return props
+
+# def script_defaults(settings):
+
+# def script_load(settings):
+
+def script_update(settings):
+    Midi.initializeFromSettings(settings)
+
+def script_unload():
+    Midi.shutdown()
+
+
+class Midi(object):
+    _instance = None
+
+    class Keys(object):
+        port = "midiDevice"
+
+    @classmethod
+    def addConfig(cls, props):
+        devicesList = obs.obs_properties_add_list(props, cls.Keys.port, "MIDI Device", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+
+        availablePorts = rtmidi.MidiIn().get_ports()
+        obs.obs_property_list_add_string(devicesList, "None", "")
+        for port in availablePorts:
+            obs.obs_property_list_add_string(devicesList, port, port)
+
+    @classmethod
+    def initializeFromSettings(cls, settings):
+        cls.shutdown()
+
+        cls._instance = Midi(
+            port=obs.obs_data_get_string(settings, cls.Keys.port),
+            handlers = [
+                TransitionHandler.fromSettings(settings),
+                RecordingHandler.fromSettings(settings)
+            ]
+        )
+
+    @classmethod
+    def shutdown(cls):
+        if not cls._instance:
+            return
+        if cls._instance.midiin:
+            cls._instance.midiin.close_port();
+        cls._instance = None
+
+    def __init__(self, port, handlers):
+        self.midiin = None
+        self.currentMidiPort = ""
+        self.handlers = handlers
+        self.openPort(port)
+
+    def openPort(self, askedPort):
+        if self.currentMidiPort == askedPort:
+            return
+
+        if self.midiin:
+            self.midiin.close_port();
+            self.currentMidiPort = ""
+
+        if askedPort == "":
+            return
+
+        try:
+            self.midiin, port_name = open_midiinput(askedPort, use_virtual=True)
+            self.midiin.set_callback(self.onMidi)
+            print("connected to " + port_name)
+            self.currentMidiPort = askedPort
+        except (EOFError, KeyboardInterrupt):
+            print("Failed to open port")
+
+    def testInput(self, cmdType, channel, value):
+        print(cmdType+"\t"+str(channel)+"\t"+str(value))
+
+        for handler in self.handlers:
+            handler.handleMidi(cmdType, channel)
+
+    def onMidi(self, event, data=None):
+        message, deltatime = event
+        if message[0] == 144: # Note On
+            self.testInput("Note", message[1], message[2])
+        elif message[0] == 176: # Control Change
+            self.testInput("CC", message[1], message[2])
+
+
 class RecordingHandler(object):
+    class Keys(object):
+        cmdType = "recordingMidiType"
+        endChannel = "endRecordingMidiChannel"
+        startChannel = "startRecordingMidiChannel"
+        pauseChannel = "pauseRecordingMidiChannel"
+        unpauseChannel = "unpauseRecordingMidiChannel"
+
+    @classmethod
+    def addConfig(cls, props):
+        addMidiTypeProp(props, cls.Keys.cmdType, "Recording midi type")
+        addNoteProp(props, cls.Keys.startChannel, "Start Recording Midi Note/Address")
+        addNoteProp(props, cls.Keys.endChannel, "End Recording Midi Note/Address")
+        addNoteProp(props, cls.Keys.pauseChannel, "Pause Recording Midi Note/Address")
+        addNoteProp(props, cls.Keys.unpauseChannel, "Unpause Recording Midi Note/Address")
+
+    @classmethod
+    def fromSettings(cls, settings):
+        return RecordingHandler(
+            cmdType=obs.obs_data_get_string(settings, cls.Keys.cmdType),
+            startChannel=obs.obs_data_get_int(settings, cls.Keys.startChannel),
+            endChannel=obs.obs_data_get_int(settings, cls.Keys.endChannel),
+            pauseChannel=obs.obs_data_get_int(settings, cls.Keys.pauseChannel),
+            unpauseChannel=obs.obs_data_get_int(settings, cls.Keys.unpauseChannel)
+        )
+
     def __init__(self, cmdType, startChannel, endChannel, pauseChannel, unpauseChannel):
         self.cmdType = cmdType
         self.startChannel = startChannel
@@ -41,6 +167,25 @@ class RecordingHandler(object):
 
 
 class TransitionHandler(object):
+    class Keys(object):
+        cmdType = "transitionToSceneMidiType"
+        transitionFirst = "transitionToSceneMidiAddress"
+        duration = "transitionTime"
+
+    @classmethod
+    def addConfig(cls, props):
+        addMidiTypeProp(props, cls.Keys.cmdType, "Transition to Scene midi type")
+        addNoteProp(props, cls.Keys.transitionFirst, "First Transition to Scene Midi Note/Address")
+        obs.obs_properties_add_int(props, cls.Keys.duration, "Transition time (MS)", 0, 5000, 1)
+
+    @classmethod
+    def fromSettings(cls, settings):
+        return TransitionHandler(
+            cmdType=obs.obs_data_get_string(settings, cls.Keys.cmdType),
+            transitionFirst=obs.obs_data_get_int(settings, cls.Keys.transitionFirst),
+            duration=obs.obs_data_get_int(settings, cls.Keys.duration)
+        )
+
     def __init__(self, cmdType, transitionFirst, duration):
         self.cmdType = cmdType
         self.transitionFirst = transitionFirst
@@ -68,120 +213,11 @@ class TransitionHandler(object):
         # obs_frontend_get_current_preview_scene
 
 
-class Midi(object):
-    instance = None
-
-    def __init__(self, port, logMidiInput, handlers):
-        self.logMidiInput = logMidiInput
-        self.midiin = None
-        self.currentMidiPort = ""
-        self.handlers = handlers
-        self.openPort(port)
-
-    def openPort(self, askedPort):
-        if self.currentMidiPort == askedPort:
-            return
-
-        if self.midiin:
-            self.midiin.close_port();
-            self.currentMidiPort = ""
-
-        if askedPort == "":
-            return
-
-        try:
-            self.midiin, port_name = open_midiinput(askedPort, use_virtual=True)
-            self.midiin.set_callback(self.onMidi)
-            print("connected to " + port_name)
-            self.currentMidiPort = askedPort
-        except (EOFError, KeyboardInterrupt):
-            print("Failed to open port")
-
-    def testInput(self, cmdType, channel, value):
-        if self.logMidiInput:
-            print(cmdType+"\t"+str(channel)+"\t"+str(value))
-
-        for handler in self.handlers:
-            handler.handleMidi(cmdType, channel)
-
-    def close(self):
-        if not self.midiin:
-            return
-        self.midiin.close_port();
-        self.midiin = None
-
-    def onMidi(self, event, data=None):
-        message, deltatime = event
-        if message[0] == 144: # Note On
-            self.testInput("Note", message[1], message[2])
-        elif message[0] == 176: # Control Change
-            self.testInput("CC", message[1], message[2])
-
-
-# defines script description 
-def script_description():
-    return '''Select preview scenes and trigger transition with a midi controller. '''
-
-def addMidiTypes(props, key, description):
+def addMidiTypeProp(props, key, description):
     listProp = obs.obs_properties_add_list(props, key, description, obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     obs.obs_property_list_add_string(listProp, "Control Change", "CC")
     obs.obs_property_list_add_string(listProp, "Note On", "Note")
 
+
 def addNoteProp(props, key, description):
     obs.obs_properties_add_int(props, key, description, 0, 127, 1)
-
-# defines user properties
-def script_properties():
-    props = obs.obs_properties_create()
-    devicesList = obs.obs_properties_add_list(props, "midiDevice", "MIDI Device", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-
-    availablePorts = rtmidi.MidiIn().get_ports()
-    obs.obs_property_list_add_string(devicesList, "", "")
-    for port in availablePorts:
-        obs.obs_property_list_add_string(devicesList, port, port)
-
-    addMidiTypes(props, "transitionToSceneMidiType", "Transition to Scene midi type")
-    addNoteProp(props, "transitionToSceneMidiAddress", "First Transition to Scene Midi Note/Address")
-    obs.obs_properties_add_int(props, "transitionTime", "Transition time (MS)", 0, 5000, 1)
-
-    addMidiTypes(props, "recordingMidiType", "Recording midi type")
-    addNoteProp(props, "startRecordingMidiAddress", "Start Recording Midi Note/Address")
-    addNoteProp(props, "endRecordingMidiAddress", "End Recording Midi Note/Address")
-    addNoteProp(props, "pauseRecordingMidiAddress", "Pause Recording Midi Note/Address")
-    addNoteProp(props, "unpauseRecordingMidiAddress", "Unpause Recording Midi Note/Address")
-
-    obs.obs_properties_add_bool(props, "logMidiInput", "Log MIDI input")
-
-    return props
-
-# def script_defaults(settings):
-
-# def script_load(settings):
-
-def script_update(settings):
-    if Midi.instance:
-        Midi.instance.close()
-        Midi.instance = None
-
-    Midi.instance = Midi(
-        port=obs.obs_data_get_string(settings, "midiDevice"),
-        logMidiInput=obs.obs_data_get_bool(settings, "logMidiInput"),
-        handlers = [
-            TransitionHandler(
-                cmdType=obs.obs_data_get_string(settings, "transitionToSceneMidiType"),
-                transitionFirst=obs.obs_data_get_int(settings, "transitionToSceneMidiAddress"),
-                duration=obs.obs_data_get_int(settings, "transitionTime")
-            ),
-            RecordingHandler(
-                cmdType=obs.obs_data_get_string(settings, "recordingMidiType"),
-                startChannel=obs.obs_data_get_int(settings, "startRecordingMidiAddress"),
-                endChannel=obs.obs_data_get_int(settings, "endRecordingMidiAddress"),
-                pauseChannel=obs.obs_data_get_int(settings, "pauseRecordingMidiAddress"),
-                unpauseChannel=obs.obs_data_get_int(settings, "unpauseRecordingMidiAddress")
-            ),
-    ])
-
-def script_unload():
-    if Midi.instance:
-        Midi.instance.close()
-        Midi.instance = None
